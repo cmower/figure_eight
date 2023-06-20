7cd42fd87ed0f735f975fd6142c23dabc7d59595
Christopher E. Mower <christopher.mower@kcl.ac.uk>
2023-06-20 10:14:25 +0100

    Add figure eight package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="figure_eight",
+    description="Simple package that generates figure of eight motion.",
+    version="0.0.0",
+    url="https://github.com/cmower/figure_eight",
+    author="Christopher E. Mower",
+    author_email="christopher.mower@kcl.ac.uk",
+    license="LGPL-3.0 license",
+    packages=["figure_eight"],
+    install_requires=["numpy"],
+)
